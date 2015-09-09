9bbd590fa6a711f3a72d14c71ba21b0278fae973
Peter B Smith <peterbutlersmith@gmail.com>
2015-09-09 00:08:55 +0000

    Basic communication with slash command

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post('/', function (req, res) {
+  console.log("Received pomo command from " + req.body.user_name + ".");
+  console.log(req.body);
+  res.send('Hi Pomo');
+})
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
@@ -11,6 +22,3 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-
-
